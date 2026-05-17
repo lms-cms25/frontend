5fe2a31189c1e24e48237c3dd28492ffa986e423
migue <migue@LAPTOP-LIQQFHEB>
2026-05-17 11:27:06 +0200

    refactor(api): remove server-only cookie dependency

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,10 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+export async function fetchWithAuth(token: string | undefined, url: string, options: RequestInit = {}) {
 
-export async function fetchWithAuth(url: string, options: RequestInit = {}) {
-  const cookieStore = await cookies();
-  const token = cookieStore.get('session_token')?.value;
 
   // Om ingen session finns, försök inte ens – redirecta direkt
   if (!token) {
-    redirect('/signin-email');
+    console.log("fetchWithAuth.ts token: " + token);
+    return null;
   }
 
   const res = await fetch(url, {
@@ -19,9 +16,10 @@ export async function fetchWithAuth(url: string, options: RequestInit = {}) {
     },
   });
 
-  // Om token har gått ut (401), rensa inte kakor här, bara redirecta.
+  // Om token har gått ut (401).
   if (res.status === 401) {
-    redirect('/signin-email');
+    console.log("fetchWithAuth.ts res.status === 401");
+    return null;
   }
 
   return res;
