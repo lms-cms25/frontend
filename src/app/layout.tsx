63b1aeae52c47e3cbef4f569156941d2345845e3
migue <migue@LAPTOP-LIQQFHEB>
2026-05-17 11:26:05 +0200

    feat(auth): load user session in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
+import { cookies } from "next/headers";
+// import Providers from "./providers";
+import { AuthProvider } from "@/context/AuthContext";
 import { Archivo } from "next/font/google";
 import "../css/globals.css";
 import "../css/theme.css";
 import "../css/buttons/buttons.css";
 import "../css/forms/forms.css";
 import "../css/labels/labels.css";
+import { fetchWithAuth } from "@/lib/api";
+
 
 const archivo = Archivo({
   subsets: ["latin"],
@@ -16,14 +21,36 @@ export const metadata: Metadata = {
   description: "Learning Management System",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const cookieStore = await cookies();
+  const token = cookieStore.get('session_token')?.value;
+
+  let user = null;
+
+  try {
+    
+    const res = await fetchWithAuth(token, `${process.env.DOTNET_AUTH_URL}/api/auth/me`)
+    
+    // console.log(res);
+    if(res?.ok){
+      user = await res.json();
+    }
+    // console.log(user);
+  } catch (error) {
+    console.log(error);
+    
+  }
+    
   return (
     <html lang="en" className={archivo.variable}>
-      <body>{children}</body>
+      <AuthProvider initialUser={user}>
+        <body>{children}</body>
+      </AuthProvider>
     </html>
   );
 }
